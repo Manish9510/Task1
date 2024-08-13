d0a3edbb685cff98b2d61f5bff95e28ac8da7c53
Manish <manishjad2810@gmail.com>
2024-08-13 11:30:29 +0530

    Code Fixed

---
+++ d3Format.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+
+const data = JSON.parse(fs.readFileSync('parsed_data.json', 'utf8'));
+
+let l2Switches = [];
+let l1Switches = [];
+
+data.forEach(switchObj => {
+    if (switchObj.hasLink) {
+        l1Switches.push({
+            switch_name: switchObj.switch_name,
+            nodes: switchObj.nodes
+        });
+    } else {
+        l2Switches.push({
+            switch_name: switchObj.switch_name
+        });
+    }
+});
+
+let l1SwitchMap = new Map();
+l1Switches.forEach(l1 => {
+    l1SwitchMap.set(l1.switch_name, l1);
+});
+
+l1Switches.forEach(l1Switch => {
+    l1Switch.nodes.forEach(node => {
+        let l0Switch = data.find(sw => sw.switch_name === node && !sw.hasLink);
+        if (l0Switch) {
+            if (!l1Switch.nodes) {
+                l1Switch.nodes = [];
+            }
+            l1Switch.nodes.push({
+                switch_name: node
+            });
+        }
+    });
+});
+
+const result = {
+    l2Switches,
+    l1Switches
+};
+
+fs.writeFileSync('result.json', JSON.stringify(result, null, 2), 'utf8');
+
+console.log('Data has been structured and saved to result.json');
---
+++ parse.js
@@ -0,0 +1,100 @@
+const fs = require('fs');
+const path = require('path');
+
+function parseSwitchData(filePath) {
+    const data = fs.readFileSync("./binfile/iblatest.unknown", 'utf8');
+    const switchData = [];
+    const switchPattern = /# IB switch no\.\s*(\d+):\s*(\S+)\s*GUID:\s*(\S+)\s*Description:\s*(.+?)\s*#/g;
+    const nodePattern = /SwitchName=(\S+)\s+Nodes=([^\n]+)/g;
+    const neighborPattern = /# IB switch no\.\s*\d+:\s*\S+\s*GUID:\s*\S+\s*Description:\s*.+?\s*#([\s\S]+?)SwitchName=/g;
+
+    let match;
+    while ((match = switchPattern.exec(data)) !== null) {
+        const switchInfo = {
+            switch_name: match[2],
+            guid: match[3],
+            description: match[4],
+            nodes: [],
+            hasLink: false
+        };
+
+        let nodeMatch;
+        while ((nodeMatch = nodePattern.exec(data)) !== null) {
+            if (nodeMatch[1] === switchInfo.switch_name) {
+                switchInfo.nodes = expandNodeRanges(nodeMatch[2].split(',').map(node => node.trim()));
+                break;
+            }
+        }
+
+        const neighborMatch = neighborPattern.exec(data);
+        if (neighborMatch) {
+            const neighborInfo = neighborMatch[1];
+            if (/Switch neighbor\s+CA\s+with/.test(neighborInfo)) {
+                switchInfo.hasLink = true;
+            }
+        }
+
+        switchData.push(switchInfo);
+    }
+    return switchData;
+}
+
+function expandNodeRanges(nodes) {
+    const expandedNodes = [];
+    let currentRange = '';
+
+    nodes.forEach(node => {
+        if (node.includes('[') && !node.includes(']')) {
+            currentRange = node;
+        } else if (!node.includes('[') && node.includes(']')) {
+            currentRange += ',' + node;
+            processRange(currentRange, expandedNodes);
+            currentRange = '';
+        } else if (currentRange) {
+            currentRange += ',' + node;
+        } else {
+            processRange(node, expandedNodes);
+        }
+    });
+
+    return expandedNodes;
+}
+
+function processRange(range, expandedNodes) {
+    const rangePattern = /(\w+)\[(.+)\]/;
+    const match = rangePattern.exec(range);
+
+    if (match) {
+        const prefix = match[1];
+        const ranges = match[2].split(',').map(r => r.trim());
+        ranges.forEach(subRange => {
+            if (subRange.includes('-')) {
+                const [start, end] = subRange.split('-').map(Number);
+                for (let i = start; i <= end; i++) {
+                    expandedNodes.push(`${prefix}${i.toString().padStart(3, '0')}`);
+                }
+            } else {
+                expandedNodes.push(`${prefix}${subRange.padStart(3, '0')}`);
+            }
+        });
+    } else {
+        const numericPattern = /(\D+)(\d+)/;
+        const numericMatch = numericPattern.exec(range);
+        if (numericMatch) {
+            const prefix = numericMatch[1];
+            const number = numericMatch[2];
+            expandedNodes.push(`${prefix}${number.padStart(3, '0')}`);
+        } else {
+            expandedNodes.push(range);
+        }
+    }
+}
+
+const inputFilePath = path.join(__dirname, 'iblatest.unknown');
+const outputFilePath = path.join(__dirname, 'parsed_data.json');
+
+const parsedData = parseSwitchData(inputFilePath);
+
+fs.writeFileSync(outputFilePath, JSON.stringify(parsedData, null, 4), 'utf8');
+
+console.log('Data has been parsed and written to parsed_data.json');
